d11ac77c9318e13dacb120bd04ea9941118a3bc5
Stefanus <stephanuswilfrid@gmail.com>
2024-08-01 15:26:19 +1000

    try to fix

--- src/pages/api/movie/[movieId]/index.ts
+++ src/pages/api/movie/[movieId]/index.ts
@@ -16,10 +16,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  if (req.method !== "GET") {
-    res.setHeader("Allow", ["GET"]);
-    return res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+  // if (req.method !== "GET") {
+  //   res.setHeader("Allow", ["GET"]);
+  //   return res.status(405).end(`Method ${req.method} Not Allowed`);
+  // }
   const { movieId } = req.query;
 
   try {
