fd6925d800ea6eb7742c14f764834af74af00bdd
Stefanus <stephanuswilfrid@gmail.com>
2024-09-12 18:22:26 +1000

    should work

--- src/components/ModalProvider.tsx
+++ src/components/ModalProvider.tsx
@@ -6,7 +6,6 @@ import LoginModal from "./Modal/LoginModal";
 import RegisterModal from "./Modal/RegisterModal";
 import TopUpModal from "./Modal/TopUpModal";
 import WithdrawModal from "./Modal/WithdrawModal";
-import CreateWatchlistModal from "./Modal/CreateWatchlistModal";
 
 const ModalProvider = () => {
  
